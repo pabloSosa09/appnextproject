3758ebbe75aafd662bec6950f02ee43fdc6db3f7
pabloSosa09 <pablososa09@gmail.com>
2025-07-29 00:30:32 -0300

    first commit

--- apptravel/components/Navbar.tsx
+++ apptravel/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 export const Navbar = () => {
   return (
-    <nav className='bg-white shadow-md py-4 border-b border-gray-200'>
+    <nav className='bg-gray-500 shadow-md py-4 border-b border-gray-200'>
         {" "}
         <div className='container mx-auto flex justify-between items-center px-6 lg:px-8'>
             <Link href={"/"}>
